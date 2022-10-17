e238d32efe7cb3e54161b0ad3078a0bb84b5a052
Jennings <jtreutel+github@protonmail.com>
2022-10-17 12:45:39 +0900

    Add k8s namespaces.

--- k8s_objects.tf
+++ k8s_objects.tf
@@ -1,14 +1,24 @@
+#Create two namespaces for each user
+
 resource "kubernetes_namespace" "user_main" {
-  count = length(var.user_list)
+  for_each = toset(var.user_list)
   metadata {
-    name = "user-${var.user_list[count.index]}"
+    name = "main-${each.key}"
   }
 }
 
+resource "kubernetes_namespace" "user_alt" {
+  for_each = toset(var.user_list)
+  metadata {
+    name = "alt-${each.key}"
+  }
+}
+
+/*
 resource "kubernetes_namespace" "user_alt" {
   count = length(var.user_list)
   metadata {
     name = "user-${var.user_list[count.index]}-alt"
   }
 }
-
+*/

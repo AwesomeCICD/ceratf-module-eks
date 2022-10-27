be1e12edc32d227a6e246b34b8b27489022ddb86
Jennings <69094303+jtreutel@users.noreply.github.com>
2022-10-27 13:34:58 +0900

    Expose nodegroup attributes, add pipeline definition, add IAM role auth support (#4)
    * Initial commit of code from se-eks-cluster-japac.
    
    * Add gitignore.
    
    * Terraform fmt.
    
    * Add test file.
    
    * Initial draft of readme.
    
    * Remove provider from module.
    
    * Rename file.
    
    * Remove backend from module.
    
    * Add some namespaces to test k8s provider.
    
    * Reorganize.
    
    * Derive region.
    
    * Add k8s namespaces.
    
    * Add kubeconfig to gitignore.
    
    * Move to latest eks module ver; simplify config; tf fmt.
    
    * Removed deprecated outputs.
    
    * Add aws_profile var to support kubeconfig creation.
    
    * Avoid checking in secrets.
    
    * Fix namespace output.
    
    * Add key creation and cert signing.
    
    * tf fmt
    
    * Comment out resources we won't use.
    
    * Add kubeconfig template for later use.
    
    * Don't check in providers.tf
    
    * Add example files.
    
    * Update README.
    
    * Expose nodegroup vars.
    
    * Add vars to docs and example.
    
    * Fix default.
    
    * Add pipeline definition.
    
    * Add templated test files.
    
    * Cleanup.
    
    * Add support for IAM role auth.
    
    * terraform fmt
    
    * Delete example since README already includes it.
    
    * Clarify resource name.
    
    * Add output.
    
    * Update README.
    
    * Add kubeconfig update output.
    
    * Update README.

--- main.tf
+++ main.tf
@@ -34,13 +34,14 @@ module "vpc" {
 
 
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "18.30.2"
-  cluster_name    = local.cluster_name
-  cluster_version = var.cluster_version
-  subnet_ids      = module.vpc.private_subnets
-
-  vpc_id = module.vpc.vpc_id
+  source                    = "terraform-aws-modules/eks/aws"
+  version                   = "18.30.2"
+  cluster_name              = local.cluster_name
+  cluster_version           = var.cluster_version
+  subnet_ids                = module.vpc.private_subnets
+  vpc_id                    = module.vpc.vpc_id
+  aws_auth_roles            = var.cluster_access_iam_role_name != "" ? local.aws_auth_roles : []
+  manage_aws_auth_configmap = true
 
   eks_managed_node_group_defaults = {
     root_volume_type                     = "gp2"
@@ -61,20 +62,22 @@ module "eks" {
       launch_template_name            = "${local.cluster_name}-ng-1"
       additional_security_group_ids   = [aws_security_group.worker_group_mgmt_one.id]
       launch_template_use_name_prefix = false #workaround for bug in 18.30.2
+      iam_role_use_name_prefix        = false #workaround for bug in 18.30.2
     },
     {
-      name                            = "${local.cluster_name}-ng-2"
-      launch_template_name            = "${local.cluster_name}-ng-2"
-      additional_security_group_ids   = [aws_security_group.worker_group_mgmt_two.id]
-      launch_template_use_name_prefix = false #workaround for bug in 18.30.2
+      name                          = "${local.cluster_name}-ng-2"
+      launch_template_name          = "${local.cluster_name}-ng-2"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      iam_role_use_name_prefix      = false #workaround for bug in 18.30.2
     }
   ]
 }
 
-resource "null_resource" "kubeconfig" {
+resource "null_resource" "cluster_creator_kubeconfig" {
 
   triggers = {
-    cluster_id = module.eks.cluster_id
+    #  cluster_id = module.eks.cluster_id
+    always_run = "${timestamp()}" #makes it run on every apply
   }
 
   provisioner "local-exec" {
--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,11 @@ output "cluster_endpoint" {
   value       = module.eks.cluster_endpoint
 }
 
+output "cluster_arn" {
+  description = "EKS cluster ARN."
+  value       = module.eks.cluster_arn
+}
+
 output "cluster_security_group_id" {
   description = "Security group ids attached to the cluster control plane."
   value       = module.eks.cluster_security_group_id
@@ -29,4 +34,19 @@ output "k8s_namespaces" {
     [for ns in kubernetes_namespace.user_main : ns.metadata[0].name],
     [for ns in kubernetes_namespace.user_alt : ns.metadata[0].name]
   )
+}
+
+output "kubeconfig_update_command" {
+  value = <<EOF
+  To update your kubeconfig file, sign in to AWS SSO via CLI and then run the following commands:
+      
+      aws eks update-kubeconfig --name ${local.cluster_name} --region ${data.aws_region.current.name}
+      kubectl config rename-context ${module.eks.cluster_arn} ${local.cluster_name}
+      kubectl config set-context ${local.cluster_name}
+  
+  To verify access to the cluster, try to list the namespaces:
+      
+      kubectl get namespaces
+
+EOF
 }

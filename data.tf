c334496a526c14a51982ca057b3345fa95f8f7b4
Jennings <jtreutel+github@protonmail.com>
2022-11-14 16:02:29 +0900

    Pull EKS access role name from global TF plan state

--- data.tf
+++ data.tf
@@ -6,11 +6,22 @@ data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
 
-data "aws_availability_zones" "available" {}
-
 data "aws_region" "current" {}
 
+data "aws_availability_zones" "available" {}
+
 data "aws_caller_identity" "current" {}
+
+# Read the remote state of the se-eks-cluster-global plan to get its outputs
+data "terraform_remote_state" "se_eks_cluster_global" {
+  backend = "s3"
+
+  config = {
+    bucket         = "se-cluster-tf"
+    region         = "us-west-2"
+    key            = "se-eks-cluster/global/terraform.tfstate"
+  }
+}
 
 locals {
   /*
@@ -19,10 +30,11 @@ locals {
    */
   cluster_name = "cera-${lookup(var.region_short_name_table, data.aws_region.current.name)}-${format("%.32s", var.cluster_suffix)}"
 
-  # Currently just maps an SSO role to a group with system:masters permission
+  # Maps the SSO role plus any additional specified roles to the system:masters group
   aws_auth_roles = concat(
     [
-      for name in [for role in data.aws_iam_role.cluster_access : role.name] : {
+      #for name in [for role in concat(var.additional_iam_role_names, [data.terraform_remote_state.se_eks_cluster_global.outputs.se_sso_iam_role]) : role.name] : {
+      for name in concat(var.additional_iam_role_names, [data.terraform_remote_state.se_eks_cluster_global.outputs.eks_access_iam_role_name]) : {
 
         "groups" : [
           "system:masters"
@@ -30,20 +42,7 @@ locals {
         "rolearn" : "arn:aws:iam::${data.aws_caller_identity.current.id}:role/${name}", # We have to strip the path since IAM role paths aren't supported.  See https://docs.aws.amazon.com/eks/latest/userguide/troubleshooting_iam.html#security-iam-troubleshoot-ConfigMap
         "username" : "aws-sso-user"
       }
-    ],
-    [
-      for name in [for role in aws_iam_role.circleci_org_access : role.name] : {
-        "groups" : [
-          "system:masters"
-        ],
-        "rolearn" : "arn:aws:iam::${data.aws_caller_identity.current.id}:role/${name}", # We have to strip the path since IAM role paths aren't supported.  See https://docs.aws.amazon.com/eks/latest/userguide/troubleshooting_iam.html#security-iam-troubleshoot-ConfigMap
-        "username" : "circleci-oidc-user"
-      }
     ]
   )
 }
 
-data "aws_iam_role" "cluster_access" {
-  for_each = toset(var.cluster_access_iam_role_names)
-  name     = each.key
-}

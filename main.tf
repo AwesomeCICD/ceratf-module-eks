6ce76bdfaf4888e3dca392b45cc1b0f1686d7dd9
Jennings <jtreutel+github@protonmail.com>
2023-06-22 11:17:08 +0900

    Upgrade VPC module to avoid breaking changes caused by latest aws_vpc resource.

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.2.0"
+  version = "5.0.0"
 
   name                 = "${local.derived_cluster_name}-vpc"
   cidr                 = "10.0.0.0/16"

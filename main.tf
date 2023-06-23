2151dc7be9681eb77a891822ce433224e8d15474
Eddie Webb <ollitech@gmail.com>
2023-06-23 09:58:02 -0400

    EBS CSI Driver: downgrade to 1.19.0build-1

--- main.tf
+++ main.tf
@@ -152,7 +152,7 @@ resource "random_string" "suffix" {
 resource "aws_eks_addon" "addons" {
   cluster_name                = module.eks.cluster_name
   addon_name                  = "aws-ebs-csi-driver"
-  addon_version               = "v1.19.0-eksbuild.2"
+  addon_version               = "v1.19.0-eksbuild.1"
   service_account_role_arn    = aws_iam_role.eks_addon_ebs_csi.arn
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "NONE"

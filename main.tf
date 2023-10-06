4343d69d196618d18315e097b994d56a3d30d214
Eddie Webb <ollitech@gmail.com>
2023-10-06 15:48:17 -0400

    asg tag fun

--- main.tf
+++ main.tf
@@ -90,20 +90,17 @@ module "eks" {
 
 #using managed node groups creates a big of a work around need for us to tag the ASGs themselvbes (not the nodes or nodegroup)
 resource "aws_autoscaling_group_tag" "cera_asg_tag_critical" {
-  for_each =  toset( module.eks.eks_managed_node_groups_autoscaling_group_names) 
-
-  autoscaling_group_name = each.value
+  autoscaling_group_name = module.eks.eks_managed_node_groups_autoscaling_group_names[0]
 
   tag {
     key   = "critical-resource"
     value = "critical-until-2024-02-01"
     propagate_at_launch = false
   }
+  depends_on = [ module.eks ]
 }
 resource "aws_autoscaling_group_tag" "cera_asg_tag_owner" {
-  for_each =  toset( module.eks.eks_managed_node_groups_autoscaling_group_names) 
-
-  autoscaling_group_name = each.value
+  autoscaling_group_name = module.eks.eks_managed_node_groups_autoscaling_group_names[0]
 
   tag {
     key   = "owner"

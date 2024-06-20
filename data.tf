96b3cebfb6a30e3487c77071b4fe8d726fe38bf3
Vijay Pandian <ananth.vijay@gmail.com>
2024-06-20 15:55:26 -0400

    Remove local aws_auth_role definition for v20 deprecation

--- data.tf
+++ data.tf
@@ -11,18 +11,5 @@ locals {
    */
   derived_cluster_name = "cera-${lookup(var.region_short_name_table, data.aws_region.current.name)}-${format("%.32s", var.cluster_suffix)}"
 
-  # Maps the SSO role plus any additional specified roles to the system:masters group
-  aws_auth_roles = concat(
-    [
-      for name in concat(var.additional_iam_role_names, [var.eks_access_iam_role_name]) : {
-
-        "groups" : [
-          "system:masters"
-        ],
-        "rolearn" : "arn:aws:iam::${data.aws_caller_identity.current.id}:role/${name}", # We have to strip the path since IAM role paths aren't supported.  See https://docs.aws.amazon.com/eks/latest/userguide/troubleshooting_iam.html#security-iam-troubleshoot-ConfigMap
-        "username" : "aws-sso-user"
-      }
-    ]
-  )
 }
 

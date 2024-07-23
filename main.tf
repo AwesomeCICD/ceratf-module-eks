9817d9875e3b54bebd73d0d484efe8c3ae126b62
Eddie Webb <ollitech@gmail.com>
2024-07-23 17:19:51 -0400

    [semver:minor] use only 2 subnets

--- main.tf
+++ main.tf
@@ -10,9 +10,9 @@ module "vpc" {
 
   name                 = "${local.derived_cluster_name}-vpc"
   cidr                 = "10.0.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  azs                  = formatlist("${data.aws_region.current.name}%s", ["a", "b"])
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
